05aca6ad0ab695bd759c21ec2a1dcca3305e521d
Andy Condon <andy.condon@koordinates.com>
2019-04-07 21:08:34 +1200

    bedtime

--- refactionjs/src/js/Header.js
+++ refactionjs/src/js/Header.js
@@ -2,21 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class Header extends React.Component {
-  testFunc() {
-    alert('hello')
-  }
-
   render() {
     return (
       <div className="header">
         <h1><Link to="/">People</Link></h1>
         <ul>
-          <li onClick={() => this.testFunc()}>Test</li>
           <li><Link to="/">Everyone</Link></li>
           <li><Link to="/male">Male</Link></li>
           <li><Link to="/female">Female</Link></li>
           <li><Link to="/over30">Over 30</Link></li>
-          <li><Link to="/under30">Under30</Link></li>
+          <li><Link to="/under30">Under 30</Link></li>
         </ul>
       </div>
     )
--- refactionjs/src/js/NameList.js
+++ refactionjs/src/js/NameList.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import data from '../db.js'
 
-const renderFindPeeps = (hash) => {
+const findPeeps = (hash) => {
   const newHash = hash.split('/')[1]
   switch(newHash) {
     case 'male':
-      return 'blokes';
-      break;
     case 'female':
-      return 'chicks';
+      const peopleGender = data.filter((person) => {
+        return person.gender === newHash
+      });
+      return renderPeeps(peopleGender);
       break;
     case 'over30':
-      return 'old'
+    case 'under30':
+      const peopleAge = data.filter((person) => {
+        if (newHash === 'over30') {
+          return person.age >= 30;
+        } else {
+          return person.age < 30;
+        };
+      })
+      return renderPeeps(peopleAge);
+      break;
     default:
-      return 'all';
+      return renderPeeps(data);
   }
   return newHash;
+}
+
+const renderPeeps = (people) => {
+  let selectedPeople = [];
+  people.map((person) => {
+    selectedPeople.push(<li className="person">{person.name} {person.age}</li>)
+  });
+  return selectedPeople;
 }
 
 class NameList extends React.Component {
@@ -24,7 +42,7 @@ class NameList extends React.Component {
     return (
       <div className="name-list">
         <ul>
-          { renderFindPeeps(window.location.hash) }
+          { findPeeps(window.location.hash) }
         </ul>
       </div>
     )
