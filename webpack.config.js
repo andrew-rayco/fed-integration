1cd03006fae6820dd0d4bd55cae440eabd55acca
Andy Condon <andy.condon@koordinates.com>
2019-04-07 16:38:15 +1200

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    entry: "./refactionjs/src/js/App.js",
+    output: {
+        path: path.resolve(__dirname, "refactionjs/public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["babel-preset-env"]
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'refactionjs/public'),
+        port: 8888
+    }
+};
