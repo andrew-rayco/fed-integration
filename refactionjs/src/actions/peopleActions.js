c938e61684ba1fbc57ec0965f4f1f03e1e620112
Andy Condon <andy.condon@koordinates.com>
2019-04-08 22:16:16 +1200

    People filter logic, connect Header component

--- refactionjs/src/actions/peopleActions.js
+++ refactionjs/src/actions/peopleActions.js
@@ -2,17 +2,48 @@ import * as types from './actionTypes';
 
 const data = '/db.json';
 
-export function receivePeople(json) {
+export function receivePeople(peopleData) {
+  console.log('peopleData', peopleData);
   return {
     type: types.RECEIVE_PEOPLE,
-    people: json
+    people: peopleData
   };
 }
 
-export function fetchPeople() {
+export function fetchPeople(id) {
   return dispatch => {
     return fetch(data)
-    .then(response => response.json())
-    .then(json => dispatch(receivePeople(json)))
+    .then(response => {
+      return response.json().then(people => {
+        let chosenPeople;
+        switch (id) {
+          case 'male':
+          case 'female':
+            chosenPeople = people.filter(person => person.gender === id);
+            return chosenPeople;
+            break;
+          case 'over30':
+          case 'under30':
+            chosenPeople = people.filter(person => {
+              if (id === 'over30') {
+                return person.age >= 30;
+              } else {
+                return person.age < 30;
+              };
+            })
+            return chosenPeople;
+            break;
+          default:
+            chosenPeople = people;
+            return chosenPeople;
+        };
+
+        return {
+          people: chosenPeople,
+          status: response.status
+        }
+      })
+    })
+    .then(result => dispatch(receivePeople(result)))
   };
 }
--- refactionjs/src/js/Header.js
+++ refactionjs/src/js/Header.js
@@ -1,28 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as peopleActions from '../actions/peopleActions';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   handleClick(e) {
-    console.log(e.target)
+    const isClickable = e.target.getAttribute('data-nav');
+    if (isClickable) {
+      this.props.peopleActions.fetchPeople(isClickable);
+    }
+    return;
   }
 
   render() {
     return (
       <div className="header" onClick={(e) => this.handleClick(e)}>
-        <h1>People</h1>
+        <h1 data-nav="everyone">People</h1>
         <ul>
-          <li>Everyone</li>
-          <li>Male</li>
-          <li>Female</li>
-          <li>Over 30</li>
-          <li>Under 30</li>
+          <li data-nav="everyone">Everyone</li>
+          <li data-nav="male">Male</li>
+          <li data-nav="female">Female</li>
+          <li data-nav="over30">Over 30</li>
+          <li data-nav="under30">Under 30</li>
         </ul>
       </div>
     )
   }
 };
 
-export default Header;
+Header.propTypes = {
+  peopleActions: PropTypes.object
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    peopleActions: bindActionCreators(peopleActions, dispatch)
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Header);
