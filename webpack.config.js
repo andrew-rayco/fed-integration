a27c51a3b9a3a619700f5b8d8596fc4b0a8a6d0a
Andy Condon <andy.condon@koordinates.com>
2019-04-08 22:26:27 +1200

    Sass webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: "./refactionjs/src/js/index.js",
-    output: {
-        path: path.resolve(__dirname, "refactionjs/public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
+  mode: 'development',
+  entry: "./refactionjs/src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "refactionjs/public"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "index.css"
+    })
+  ],
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env", "babel-preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
           {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["babel-preset-env", "babel-preset-react"]
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [{
-                        loader: "style-loader" // creates style nodes from JS strings
-                    },
-                    {
-                        loader: "css-loader" // translates CSS into CommonJS
-                    },
-                    {
-                        loader: "sass-loader" // compiles Sass to CSS
-                    }
-                ]
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: path.resolve(__dirname, "refactionjs/public")
             }
+          },
+          "css-loader"
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'refactionjs/public'),
-        port: 8888
-    }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'refactionjs/public'),
+    port: 8888
+  }
 };
